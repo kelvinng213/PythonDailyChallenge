e777e9ba41b8ccc0fab8b188585247dd28217c63
kelvinng213 <ngkelvin9@gmail.com>
2020-08-25 13:45:11 +0800

    Day 2 Problem

--- Daily Challenge Question/Day2Challenge.py
+++ Daily Challenge Question/Day2Challenge.py
@@ -1,11 +1,11 @@
-# Create a function that estimates the weight loss of a person using a certain weight loss program with their gender, current weight and how many weeks they plan to do the program as input. If the person follows the weight loss program, men can lose 1.5% of their body weight per week while women can lose 1.2% of their body weight per week. 
+# Create a function that estimates the weight loss of a person using a certain weight loss program 
+# with their gender, current weight and how many weeks they plan to do the program as input. 
+# If the person follows the weight loss program, men can lose 1.5% of their body weight per week while
+# women can lose 1.2% of their body weight per week. 
 
 # The possible inputs are:
-
 # Gender: 'M' for Male, 'F' for Female
-
 # Current weight: integer above 0
-
 # Number of weeks: integer above 0
 
 # Return the estimated weight after the specified number of weeks.
